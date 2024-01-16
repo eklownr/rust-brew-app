300fb3f6af0be322b872267bbbc806152414d075
eklownr <eklownr@gmail.com>
2024-01-16 20:49:47 +0100

    add frontend

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 slint::include_modules!();
 
+const ALK_CONST: f64 = 0.132;
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-
     let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
+    ui.on_calc_volym(move |string| {
         let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+        let og: f64 = string.trim().parse().unwrap();
+        let vol: f64 = og * ALK_CONST;
+        let result: String = format!("{:.2} Volym %" ,vol);
+        ui.set_results(result.into());
     });
 
     ui.run()
